eec2df451df4c95a071f0d29f94f7863de89ac03
Franklin Jeng <franklinjjeng@gmail.com>
2017-01-23 15:41:51 -0800

    Simple TicTacToe game created

--- public/client/app.js
+++ public/client/app.js
@@ -1,6 +1,18 @@
 var simpleGame = angular.module('simpleGame', []);
 
 simpleGame.factory('games', function($http) {
+
+  var playerScore = {scores:
+    {
+      player1: 0,
+      player2: 0
+    }
+  }
+
+  var getScore = function() {
+    return playerScore;
+  }
+
   var gameBoard = {board: 
     [
       [{id: 0, occupy: '_  _', coord: [0, 0]}, {id: 1, occupy: '_  _', coord: [0, 1]}, {id: 2, occupy: '_  _', coord: [0, 2]}],
@@ -28,25 +40,107 @@ simpleGame.factory('games', function($http) {
       playerTurn = 0;
     }
     return playerSpace[playerTurn];
-  }
+  };
+
+  var checkWinner = function(player) {
+    for (var i = 0; i < gameBoard.board.length; i++) {
+      if(rowWin(player, i)) {
+        return true;
+      } else if (colWin(player, i)) {
+        return true;
+      }
+    }
+    if (majorDiagWin(player)) {
+      return true;
+    } else if (minorDiagWin(player)) {
+      return true;
+    }
+    return false;
+  };
+
+  var rowWin = function(player, row) {
+    for (var i = 0; i < gameBoard.board.length; i++) {
+      if (gameBoard.board[row][i].occupy !== player) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var colWin = function(player, col) {
+    for (var i = 0; i < gameBoard.board.length; i++) {
+      if (gameBoard.board[i][col].occupy !== player) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var majorDiagWin = function(player) {
+    for (var i = 0; i < gameBoard.board.length; i++) {
+      if (gameBoard.board[i][i].occupy !== player) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var minorDiagWin = function(player) {
+    for (var i = 0; i < gameBoard.board.length; i++) {
+      if (gameBoard.board[i][gameBoard.board.length - 1 - i].occupy !== player) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  var updateScore = function(player) {
+    if (player === '_X_') {
+      playerScore.scores.player1++;
+      return playerScore.scores.player1;
+    } else {
+      playerScore.scores.player2++;
+      return playerScore.scores.player2;
+    }
+  };
+
+  var clearBoard = function() {
+    for (var i = 0; i < gameBoard.board.length; i++) {
+      for (var j = 0; j < gameBoard.board.length; j++) {
+        gameBoard.board[i][j].occupy = '_  _';
+      }
+    }
+  };
+
+  var newGame = function(player) {
+    updateScore(player);
+    getScore();
+    clearBoard();
+    getBoard();
+  };
 
   return {
+    playerScore: playerScore,
+    getScore: getScore,
     gameBoard: gameBoard,
     playerSpace: playerSpace,
     playerTurn: playerTurn,
     getBoard: getBoard,
     occupyBoard: occupyBoard,
-    changePlayer: changePlayer
+    changePlayer: changePlayer,
+    checkWinner: checkWinner,
+    rowWin: rowWin,
+    colWin: colWin,
+    majorDiagWin: majorDiagWin,
+    minorDiagWin: minorDiagWin,
+    updateScore: updateScore,
+    clearBoard: clearBoard,
+    newGame: newGame,
   }
 });
 
 simpleGame.controller('gamesController', function($scope, games) {
   $scope.blank = '_  _';
-  // $scope.player1 = ' X ';
-  // $scope.player1 = ' O ';
-  // $scope.playerTurn = 'p1';
-  $scope.playerSpace = ['_X_', '_O_'];
-  $scope.playerTurn = 0;
 
   var init = function() {
     $scope.board = games.getBoard();
@@ -54,18 +148,29 @@ simpleGame.controller('gamesController', function($scope, games) {
     $scope.row1 = $scope.board.board[1];
     $scope.row2 = $scope.board.board[2];
     $scope.player = games.changePlayer();
+    $scope.playerScore = games.getScore();
   }
 
   $scope.playerMove = function(input) {
     if (input.occupy !== '_  _') {
-      console.log('invalid move, space occupied');
+      console.log('Invalid move, space occupied');
       return;
     } else {
       games.occupyBoard(input.coord, $scope.player);
       $scope.board = games.getBoard();
+      if (games.checkWinner($scope.player)) {
+        console.log($scope.player + ' wins!');
+        games.newGame($scope.player);
+      }
       $scope.player = games.changePlayer();
     }
   }
+
+  // Hooked up to button to test functions when clicked
+  $scope.devFunctions = function() {
+    console.log('test not hooked up');
+    // console.log($scope.playerScore);
+  };
 
   init();
 
