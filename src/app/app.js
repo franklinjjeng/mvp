ed3e708a6fc0d78a31b06370eff7bf01988c366d
Franklin Jeng <franklinjjeng@gmail.com>
2017-08-30 12:12:55 -0700

    (cleanup) clean up files not used

--- src/app/app.js
+++ src/app/app.js
@@ -8,7 +8,7 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <h1>Hello</h1>
+      <h1>Hello WORLD!</h1>
     );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,19 +2,28 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src/app', 'app.jsx'),
+  entry: path.resolve(__dirname, 'src/app', 'app.js'),
   output: {
     path: path.resolve(__dirname, 'src/public/build'),
     filename: 'bundle.js'
+  },
+  resolve: {
+    modulesDirectories: ['node_modules', 'src'],
+    extensions: ['', '.js', '.jsx', '.css'],
   },
   module: {
     loaders: [
       {
         test: /\.jsx?/,
         include: path.resolve(__dirname, 'src/app'),
+        exclude: /node_modules/,
         loader: 'babel'
       }
     ]
   },
-  devtool: 'source-map'
+  devtool: 'source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin(),
+  ]
 };
